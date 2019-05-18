aedb1fb1a7e3c9170aad20436714c40e077a9dbe
Sebi1006 <sebastian.reinbold@htwg-konstanz.de>
2019-05-18 14:08:01 +0200

    Preparing for new structure

--- game/src/main/scala/de/htwg/sa/minesweeper/model/gridcomponent/gridbaseimpl/InjectedGrid.scala
+++
@@ -1,8 +0,0 @@
-package de.htwg.sa.minesweeper.model.gridcomponent.gridbaseimpl
-
-import com.google.inject.Inject
-import com.google.inject.name.Named
-
-class InjectedGrid @Inject()(@Named("Default") gridSize: Int) extends Grid(gridSize) {
-  new Grid(gridSize)
-}
